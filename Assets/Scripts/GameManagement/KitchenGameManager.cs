75942059e9907d183029eca411b3016a1ff51c3c
jaypets <jpsmith8603@gmail.com>
2023-04-01 17:34:51 -0400

    Bug Fixes

--- Assets/Scripts/GameManagement/KitchenGameManager.cs
+++ Assets/Scripts/GameManagement/KitchenGameManager.cs
@@ -22,7 +22,7 @@ public class KitchenGameManager : MonoBehaviour
     private State state;
     private float countdownToStartTimer = 3f;
     private float gamePlayingTimer;
-    private float gamePlayingTimerMax = 60f;
+    private float gamePlayingTimerMax = 180f;
     private bool isGamePaused = false;
 
     private void Awake()
